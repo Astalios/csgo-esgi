fc8f9ae908118cb5a9b6018a11cabf29dbcfa4fe
hhk <louis.ricour@gmail.com>
2020-10-18 22:01:28 +0200

    bootstrap > tailwind (trop long a  apprendre)

--- site/app/csgo-esgi/resources/views/layout.blade.php
+++ site/app/csgo-esgi/resources/views/layout.blade.php
@@ -4,10 +4,34 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Logo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Actualités</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav my-2 my-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Se connecter</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
     <main class="container-fluid">
         @include('flash::message')
         @yield('content')
     </main>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
-
